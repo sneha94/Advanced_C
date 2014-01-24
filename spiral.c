4043d319696eb035ab57b3b237039c4baca1f3cf
sneha94 <snehakulkarni094@gmail.com>
2014-01-24 20:05:38 +0530

    not competed

---
+++ spiral.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+void spiral_horizontal(int *a)
+{
+int i,j;
+for( i=0;i<4;i++)
+{
+a[i][0]=i+1;
+}
+a[3][0]=k;
+for(j=0;j<4;j++)
+{
+a[3][j]=j+4;
+
+
+}
+j+4=k;
+for( i=3;i>=0;i--)
+{
+
+a[i][3]=k;
+k++;
+
+}
+for(j=3;j>=0;j--)
+{
+
+a[i][3]=k;
+k++;
+
+}
+}
+
+int main()
+{
+int a[4][4];
+spiral(a);
+
